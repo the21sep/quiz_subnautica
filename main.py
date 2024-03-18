73d782501f3f712e18f735eee43b0c54298b76a3
the21sep <163855805+the21sep@users.noreply.github.com>
2024-03-18 13:40:44 +0000

    первый

---
+++ main.py
@@ -0,0 +1,82 @@
+print('Добро пожаловать на викторину по игре subnautica!')
+print('Ответь на следущие вопросы:')
+questions= [
+'1. Какой самый быстрый транспорт в игре?',
+'2. Какой самый большой транспорт в игре?',
+'3. Название левиофана который обитает рядом с авророй?',
+'4. Какой самый большой левиофан в игре?',
+'5. Какой самый маленький левиофан в игре?',
+'6. Что требуется чтобы закончить игру?',
+'7. Название космического корабля на котором упал главный герой игры?'
+]
+answers = [
+'Мотылёк',
+'Циклоп',
+'Жнец',
+'Морской император',
+'Рифоспин',
+'Отключить пушку ПВО и улететь на ракете',
+'Аврора'
+]
+score = 0
+print(questions[0])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[0].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+print(questions[1])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[1].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+
+print(questions[2])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[2].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+
+print(questions[3])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[3].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+
+print(questions[4])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[4].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+
+print(questions[5])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[5].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно.")
+
+print(questions[6])
+user_answer = input("Введи свой ответ: ")
+if user_answer.lower() == answers[6].lower():
+    print("Правильно!")
+    score = score + 1
+else:
+    print("Неправильно")
+
+if score>5:
+    print("Это отличный результат! Ты профессионал в сабнатике.")
+elif score>3:
+    print("Неплохой результат! Ты многое знаешь но не всё о этой игре.")
+else:
+    print("Видимо ты новичёк, тебе ещё многое предстоит узнать об этой игре!.")
